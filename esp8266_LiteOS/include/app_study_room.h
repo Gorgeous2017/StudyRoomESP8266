281eda69fc5a7bf5a31fe701cd3941dc885fadf4
Gump <870705273@qq.com>
2020-01-30 17:26:58 +0800

    refactor: change some format

--- esp8266_LiteOS/include/app_study_room.h
+++ esp8266_LiteOS/include/app_study_room.h
@@ -1,50 +1,37 @@
+/**
+ * ******************************************************************************
+ * @file app_study_room.h
+ * @author Gump
+ * @version V0.1
+ * @date 2020-01-29
+ * @brief 
+ * 
+ * @copyright Copyright (c) <2020> <HANSHAN NORMAL UNIVERSITY(CHAOZHOU)> All rights Reserved.
+ *   Media Recognition and Intelligent System Team
+ * 
+ * ******************************************************************************
+ */
 
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __APP_STUDY_ROOM_H
+#define __APP_STUDY_ROOM_H 
+
+/* Includes ------------------------------------------------------------------*/
 #include <stddef.h>
 #include <stdint.h>
 
-#include <at.h>
-#include <boudica150_oc.h>
-#include <los_dev.h>
-#include <oc_lwm2m_agent.h>
-#include <osport.h>
-
-typedef unsigned char int8u;
-typedef char int8s;
-typedef unsigned short int16u;
-typedef short int16s;
-typedef unsigned char int24u;
-typedef char int24s;
-typedef int int32s;
-typedef char string;
-typedef char array;
-typedef char varstring;
-typedef char variant;
-
-#define cn_app_bands "5,8,20"
-#define cn_app_plmn "46011";
-#define cn_app_apn "1,\"IP\",\"HUAWEI.COM\""
-
-#define HUAWEI_IOT_SERVER "49.4.85.232"
-#define HUAWEI_IOT_PORT "5683"
-
-short int Lux;
-
+/* Exported typedef -----------------------------------------------------------*/
 /**
- * @addtogroup DecodeMessageId
- * @{
+ * @brief 用电器类型枚举体
  * 
  */
-#define MID_SET_FAN 0x0					/*!< 风扇控制指令 */
-#define MID_SET_LIGHT 0x1				/*!< 电灯控制指令 */
-#define MID_SET_CURTAIN 0x2				/*!< 窗帘控制指令 */
-#define MID_SET_AC 0x3					/*!< 空调控制指令 */
-#define MID_report_room_one_message 0x4 /*!< 房间一的环境信息 */
-#define MID_report_room_one_status 0x5  /*!< 房间一的用电器状态信息 */
-
-/**
- * @}
- * 
- */
+enum Device
+{
+	DEVICE_FAN = 0x00,	 /*!< 风扇用电器 */
+	DEVICE_LIGHT = 0x01,   /*!< 电灯用电器 */
+	DEVICE_CUTRAIN = 0x02, /*!< 窗帘用电器 */
+	DEVICE_AC = 0x03	   /*!< 空调用电器 */
+};
 
 /**
  * @brief 云端下发的用电器控制信息
@@ -58,22 +45,6 @@ typedef struct
 	uint8_t deviceSwitch;
 } DeviceController;
 
-// typedef struct
-// {
-//     int8u messageId;
-//     int8u room;
-//     int8u fanNo;
-//     int8u fanGear;
-// } tag_app_SET_FAN;
-
-// typedef struct
-// {
-//     int8u messageId;
-//     int8u room;
-//     int8u acNo;
-//     int8u acTemp;
-// } tag_app_SET_AC;
-
 /**
  * @brief 室内环境信息结构体
  * 
@@ -86,14 +57,6 @@ typedef struct
 	uint8_t humi;			/*!< 环境湿度 */
 	uint8_t noise;			/*!< 环境噪音 */
 } RoomMessage;
-
-enum Device
-{
-	DEVICE_FAN = 0x00,	 /*!< 风扇用电器 */
-	DEVICE_LIGHT = 0x01,   /*!< 电灯用电器 */
-	DEVICE_CUTRAIN = 0x02, /*!< 窗帘用电器 */
-	DEVICE_AC = 0x03	   /*!< 空调用电器 */
-};
 
 /**
  * @brief 室内用电器状态信息结构体
@@ -112,47 +75,70 @@ typedef struct
 	variant acStatus[6];	  /*!< 空调状态信息 */
 } RoomStatus;
 
-// handle all the Big endian and Little endian problem
-typedef unsigned short uint16;
-typedef unsigned long uint32;
+/* Exported constants --------------------------------------------------------*/
+#define cn_app_bands "5,8,20"
+#define cn_app_plmn "46011";
+#define cn_app_apn "1,\"IP\",\"HUAWEI.COM\""
+
+/* 信息来源：华为IoT平台-应用-对接信息-设备接入信息 */
+#define HUAWEI_IOT_SERVER "49.4.85.232" /*!< 华为IoT平台服务器地址 */
+#define HUAWEI_IOT_PORT "5683" /*!< LWM2M(CoAP) 无DTLS形式接入平台的端口 */
+
+/**
+ * @addtogroup DecodeMessageId
+ * @brief 华为IoT平台|编解码插件所用的唯一messageId，用以区分不同的平台消息
+ * @{
+ * 
+ */
+#define MID_SET_FAN 0x0					/*!< 风扇控制指令 */
+#define MID_SET_LIGHT 0x1				/*!< 电灯控制指令 */
+#define MID_SET_CURTAIN 0x2				/*!< 窗帘控制指令 */
+#define MID_SET_AC 0x3					/*!< 空调控制指令 */
+#define MID_report_room_one_message 0x4 /*!< 房间一的环境信息 */
+#define MID_report_room_one_status 0x5  /*!< 房间一的用电器状态信息 */
+
+/**
+ * @}
+ * 
+ */
+
+
+/* Exported function -----------------------------------------------*/
+
+#endif /* __APP_STUDY_ROOM_H */ 
+/********************************** END OF FILE *******************************/
+
+typedef unsigned char int8u;
+typedef char int8s;
+typedef unsigned short int16u;
+typedef short int16s;
+typedef unsigned char int24u;
+typedef char int24s;
+typedef int int32s;
+typedef char string;
+typedef char array;
+typedef char varstring;
+typedef char variant;
+
+short int Lux;
+
+// typedef struct
+// {
+//     int8u messageId;
+//     int8u room;
+//     int8u fanNo;
+//     int8u fanGear;
+// } tag_app_SET_FAN;
+
+// typedef struct
+// {
+//     int8u messageId;
+//     int8u room;
+//     int8u acNo;
+//     int8u acTemp;
+// } tag_app_SET_AC;
 
 #define Swap16(A) ((((uint16)(A)&0xff00) >> 8) | (((uint16)(A)&0x00ff) << 8))
 #define Swap32(A) \
 	((((uint32)(A)&0xff000000) >> 24) | (((uint32)(A)&0x00ff0000) >> 8) | (((uint32)(A)&0x0000ff00) << 8) | (((uint32)(A)&0x000000ff) << 24))
 
-int app_report_task_entry()
-{
-	int ret = -1;
-	tag_oc_config_param oc_param;
-
-	// System frame work initilized here, which means you could use the driver framwork, good luck~!
-	los_driv_init();
-
-	// AT interface initialize: UART 2
-	extern bool_t uart_at_init(s32_t baudrate);
-	uart_at_init(9600);
-
-	// Do the AT module initialize: AT FRAMEWORK
-	los_at_init("atdev");
-
-	// START APPLICATION INITIALIZE HERE
-	oc_lwm2m_agent_init();
-	boudica150_init(NULL, NULL, cn_app_bands);
-	oc_param.server = cn_app_server;
-	oc_param.port = cn_app_port;
-
-	// success, so we could receive and send now
-	if (0 == oc_lwm2m_config(&oc_param))
-	{
-		// install a dealer for the led message received
-		oc_lwm2m_install_msgdealer(app_msg_deal, NULL, 0);
-
-		while (1)
-		{
-			report_data_handler();
-			ret = 0;
-			task_sleepms(10 * 1000);
-		}
-	}
-	return ret;
-}
--- esp8266_LiteOS/include/nb_bc35.h
+++ esp8266_LiteOS/include/nb_bc35.h
@@ -1,42 +1,35 @@
-/*----------------------------------------------------------------------------
- * Copyright (c) <2016-2018>, <Huawei Technologies Co., Ltd>
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- * 1. Redistributions of source code must retain the above copyright notice, this list of
- * conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice, this list
- * of conditions and the following disclaimer in the documentation and/or other materials
- * provided with the distribution.
- * 3. Neither the name of the copyright holder nor the names of its contributors may be used
- * to endorse or promote products derived from this software without specific prior written
- * permission.
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *---------------------------------------------------------------------------*/
-/*----------------------------------------------------------------------------
- * Notice of Export Control Law
- * ===============================================
- * Huawei LiteOS may be subject to applicable export control laws and regulations, which might
- * include those applicable to Huawei LiteOS of U.S. and the country in which you are located.
- * Import, export and usage of Huawei LiteOS in any manner by you shall be in compliance with such
- * applicable export control laws and regulations.
- *---------------------------------------------------------------------------*/
+/**
+ * ******************************************************************************
+ * @file nb_bc35.h
+ * @author Gump
+ * @version V0.1
+ * @date 2020-01-29
+ * @brief 
+ * 
+ * @copyright Copyright (c) <2020> <HANSHAN NORMAL UNIVERSITY(CHAOZHOU)> All rights Reserved.
+ *   Media Recognition and Intelligent System Team
+ * 
+ * ******************************************************************************
+ */
 
-#ifndef __NB_NEUL_BC95_H__
-#define __NB_NEUL_BC95_H__
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __NB_BC35_H
+#define __NB_BC35_H 
 
-#include "at_frame/at_main.h"
+/* Includes ------------------------------------------------------------------*/
 
+/* Exported typedef -----------------------------------------------------------*/
+typedef struct _socket_info_t
+{
+	int socket;
+	short localport;
+	char localip[IP_LEN];
+	short remoteport;
+	char remoteip[IP_LEN];
+	bool used_flag;
+}socket_info;//struct to save socket info
+
+/* Exported constants --------------------------------------------------------*/
 #define AT_MODU_NAME            "nb_neul95"
 
 #define AT_NB_LINE_END 			"\r\n"
@@ -52,8 +45,6 @@
 #define AT_LINE_END 		    "\r\n"
 #define AT_CMD_BEGIN		    "\r\n"
 
-
-
 #define AT_USART_PORT       3
 #define AT_BUARDRATE        9600
 #define AT_CMD_TIMEOUT      10000    //ms
@@ -61,12 +52,8 @@
 
 #define NB_STAT_LOCALPORT   56
 
-
 #define MAX_SOCK_NUM        5
 #define UDP_PROTO           17
-
-
-
 
 #if defined STM32F103xE
 #define MAX_AT_USERDATA_LEN (1024*2)
@@ -77,16 +64,8 @@
 #define AT_MAX_PAYLOADLEN     512
 
 #define IP_LEN 16
-typedef struct _socket_info_t
-{
-    int socket;
-    short localport;
-    char localip[IP_LEN];
-    short remoteport;
-    char remoteip[IP_LEN];
-    bool used_flag;
-}socket_info;//struct to save socket info
 
+/* Exported function -----------------------------------------------*/
 int str_to_hex(const char *bufin, int len, char *bufout);
 int32_t nb_set_cdpserver(char* host, char* port);
 int32_t nb_hw_detect(void);
@@ -101,4 +80,11 @@ int32_t nb_recv_timeout(int32_t id , uint8_t  *buf, uint32_t len,char* ipaddr,in
 int32_t nb_cmd_match(const char *buf, char* featurestr,int len);
 void nb_step(void);
 void nb_reattach(void);
-#endif
+
+#endif /* __NB_BC35_H */ 
+/********************************** END OF FILE *******************************/
+
+
+
+
+
