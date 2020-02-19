b754263d9f3acec4efc958cd1cf85ee128abd4d8
Gump <870705273@qq.com>
2020-02-19 10:16:17 +0800

    refactor: delete test scope

--- esp8266_centre_NB/user/app_study_room.c
+++ esp8266_centre_NB/user/app_study_room.c
@@ -164,53 +164,53 @@ void StudyRoom_StatusToHex(uint8 room_no, uint8 *out_hexstr ) {
 	
 }
 
-int sensor_controll_handler()
-{
-// Define data report struct, all members come from devicemodel file
+// int sensor_controll_handler()
+// {
+// // Define data report struct, all members come from devicemodel file
 
 
-	// if (semp_pend(s_rcv_sync, 0x7fffffff))
-	// {
-	// 	uint8_t msgid = s_rcv_buffer[0];
-	// 	switch (msgid)
-	// 	{
+// 	// if (semp_pend(s_rcv_sync, 0x7fffffff))
+// 	// {
+// 	// 	uint8_t msgid = s_rcv_buffer[0];
+// 	// 	switch (msgid)
+// 	// 	{
 
-	// 	default:
-	// 		break;
-	// 	}
-	// }
-}
+// 	// 	default:
+// 	// 		break;
+// 	// 	}
+// 	// }
+// }
 
-int report_data_handler()
-{
-	DeviceController SET_LIGHT;
-	DeviceController SET_CURTAIN;
-	DeviceController SET_FAN;
-	DeviceController SET_AC;
-	RoomMessage report_room_one_message;
-	RoomStatus report_room_one_status;
+// int report_data_handler()
+// {
+// 	DeviceController SET_LIGHT;
+// 	DeviceController SET_CURTAIN;
+// 	DeviceController SET_FAN;
+// 	DeviceController SET_AC;
+// 	RoomMessage report_room_one_message;
+// 	RoomStatus report_room_one_status;
 
-/********** code area for report data to IoT cloud  **********/
+// /********** code area for report data to IoT cloud  **********/
 
-/********** code area end  **********/
+// /********** code area end  **********/
 
-// virtual sensor data example
-	report_room_one_message.messageId = MID_report_room_one_message;
-	report_room_one_message.peopleFlowrate = 0x1;
-	report_room_one_message.temp = 0x1;
-	report_room_one_message.humi = 0x1;
-	report_room_one_message.noise = 0x1;
-	oc_lwm2m_report((uint8_t *)&report_room_one_message, sizeof(report_room_one_message), 1000);
+// // virtual sensor data example
+// 	report_room_one_message.messageId = MID_report_room_one_message;
+// 	report_room_one_message.peopleFlowrate = 0x1;
+// 	report_room_one_message.temp = 0x1;
+// 	report_room_one_message.humi = 0x1;
+// 	report_room_one_message.noise = 0x1;
+// 	oc_lwm2m_report((uint8_t *)&report_room_one_message, sizeof(report_room_one_message), 1000);
 
-	report_room_one_status.messageId = MID_report_room_one_status;
-	report_room_one_status.fanNum = 0x1;
-	report_room_one_status.fanStatus[0] = 9;
-	report_room_one_status.lightNum = 0x1;
-	report_room_one_status.lightStatus[0] = 9;
-	report_room_one_status.curtainNum = 0x1;
-	report_room_one_status.curtainStatus[0] = 9;
-	report_room_one_status.acNum = 0x1;
-	report_room_one_status.acStatus[0] = 9;
-	oc_lwm2m_report((uint8_t *)&report_room_one_status, sizeof(report_room_one_status), 1000);
+// 	report_room_one_status.messageId = MID_report_room_one_status;
+// 	report_room_one_status.fanNum = 0x1;
+// 	report_room_one_status.fanStatus[0] = 9;
+// 	report_room_one_status.lightNum = 0x1;
+// 	report_room_one_status.lightStatus[0] = 9;
+// 	report_room_one_status.curtainNum = 0x1;
+// 	report_room_one_status.curtainStatus[0] = 9;
+// 	report_room_one_status.acNum = 0x1;
+// 	report_room_one_status.acStatus[0] = 9;
+// 	oc_lwm2m_report((uint8_t *)&report_room_one_status, sizeof(report_room_one_status), 1000);
 
-}
+// }
